4c499eb58cf3673ad40a7ba410f81dac227159a8
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 21:37:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   game = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => [ "black", "white" ],
+      :colors => [ "Black", "White" ],
       :players => [ 
         { :player_name => "Alan Anderson" , :number => 0 , :shoe => 16 , :points => 22 , :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
         { :player_name => "Reggie Evans" , :number => 30 , :shoe => 14 , :points => 12 , :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
@@ -12,9 +12,15 @@ def game_hash
         { :player_name => "Jason Terry" , :number => 31 , :shoe => 15 , :points => 19 , :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
           ]
     }, :away => {
-      :team_name => "teamname",
-      :colors => { },
-      :players => [ :player1 , :player2 , :player3 , :player4 , :player5 ]
+      :team_name => "Charlotte Hornets",
+      :colors => [ "Turquoise", "Purple" ],
+      :players => [
+        { :player_name => "Jeff Adrien" , :number => 4 , :shoe => 18 , :points => 10 , :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
+        { :player_name => "Bismack Biyombo" , :number => 0 , :shoe => 16 , :points => 12 , :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10},
+        { :player_name => "DeSagna Diop" , :number => 2 , :shoe => 14 , :points => 24 , :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
+        { :player_name => "Ben Gordon" , :number => 8 , :shoe => 15 , :points => 33 , :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
+        { :player_name => "Jason Terry" , :number => 31 , :shoe => 15 , :points => 19 , :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}        
+          ]
     }
   }
   game
