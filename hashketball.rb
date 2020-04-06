74cc73c039539495e8a6978393247e3153d76e66
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 21:29:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,9 +4,8 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => [ "black", "white" ],
-      :players => [ {
-        :player_name => "Alan Anderson" , :number => 0 , :shoe => 16 , :points => 22 , :rebounds => 12
-      }, 
+      :players => [ 
+        { :player_name => "Alan Anderson" , :number => 0 , :shoe => 16 , :points => 22 , :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
         :player2 => {} , :player3 => {}, :player4 => {}, :player5 => {} ]
     }, :away => {
       :team_name => "teamname",
